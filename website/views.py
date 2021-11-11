89a3cf0b05b7180c44a39fb9dceb3d098e95c627
Akash Bagchi <72804789+akashbagchi@users.noreply.github.com>
2021-11-11 21:00:40 +0530

    Update views.py

--- website/views.py
+++ website/views.py
@@ -1,8 +1,17 @@
 from flask import Blueprint, render_template, request, flash, redirect, url_for
 from werkzeug.utils import redirect
-from werkzeug.security import generate_password_hash, check_password_hash
+from werkzeug.security import generate_password_hash
 from .models import User
 from . import db
+import smtplib
+
+sender_email = "confirmemailtest1@gmail.com"
+password_email = "pass123pass"
+message = "This e-mail was sent from akashbagchi2710.pythonanywhere.com to confirm your sign-up. Thank you for visiting!"
+
+server = smtplib.SMTP("smtp.gmail.com", 587)
+server.starttls()
+server.login(sender_email, password_email)
 
 views = Blueprint('views', __name__)
 
@@ -17,7 +26,7 @@ def home():
         user = User.query.filter_by(email=email).first()
         if user:
             flash('Account already exists', category='error')
-            return redirect(url_for("auth.login"))
+            return redirect(url_for("views.home"))
         elif len(email) < 4:
             flash('Email must be greater than 3 characters.', category='error')
         elif len(firstName) <2:
@@ -28,6 +37,10 @@ def home():
             new_user = User(email=email, firstName=firstName, password=generate_password_hash(password1, method='sha256'))
             db.session.add(new_user)
             db.session.commit()
+
+            server.sendmail(sender_email, email, message)
+            print("Email sent")
+
             flash('E-mail sent!', category='success')
             return redirect(url_for('views.home'))
 
