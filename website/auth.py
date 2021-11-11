90bec15fb033cca1d736afaeb2970405de4e844c
Akash Bagchi <72804789+akashbagchi@users.noreply.github.com>
2021-11-11 20:59:54 +0530

    Update auth.py

--- website/auth.py
+++ website/auth.py
@@ -1,11 +1,9 @@
 from flask import Blueprint, render_template, request, flash, redirect, url_for
-from .models import Feedback, User
-from werkzeug.security import generate_password_hash, check_password_hash
+from .models import Feedback
 from . import db
-from flask_login import login_user, login_required, logout_user, current_user
-
 
 auth = Blueprint('auth', __name__)
+loopResult = ""
 
 @auth.route('/feedback', methods=['GET', 'POST'])
 def feedback():
@@ -28,7 +26,7 @@ def feedback():
             new_feedback = Feedback(email=email, firstName=firstName, feedbackText=feedbackText)
             db.session.add(new_feedback)
             db.session.commit()
+
             flash('Feedback submitted!', category='success')
-            return redirect(url_for('auth.feedback'))
 
     return render_template("feedback.html")
